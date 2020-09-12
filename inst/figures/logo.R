7a683bf73cb3a9b375b341280664f1697af7fd3c
nathaneastwood <nathan.eastwood@icloud.com>
2020-09-12 23:24:28 +0200

    doc: Add initial documentation

---
+++ inst/figures/logo.R
@@ -0,0 +1,14 @@
+hexSticker::sticker(
+  system.file("figures/barclays_lion.png", package = "fpl"),
+  package = "",
+  s_x = 1, s_y = 1, s_width = 0.7, s_height = 0.7,
+  p_y = 1.5,
+  p_color = "#37003C",
+  h_fill = "#00FF87",
+  h_color = "#37003C",
+  filename = "man/figures/logo.png",
+  url = "nathaneastwood/fpl",
+  u_color = "#37003C",
+  u_size = 2,
+  dpi = 1200
+)

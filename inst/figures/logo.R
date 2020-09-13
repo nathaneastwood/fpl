a3620fb3cc244d37cd88c62ee9c093f3247909a0
nathaneastwood <nathan.eastwood@icloud.com>
2020-09-13 22:24:28 +0200

    fix: Correct aspect ratio of logo

--- inst/figures/logo.R
+++ inst/figures/logo.R
@@ -1,7 +1,7 @@
 hexSticker::sticker(
   system.file("figures/barclays_lion.png", package = "fpl"),
   package = "",
-  s_x = 1, s_y = 1, s_width = 0.7, s_height = 0.7,
+  s_x = 1, s_y = 1, s_width = 0.8, s_height = 0.8,
   p_y = 1.5,
   p_color = "#37003C",
   h_fill = "#00FF87",
@@ -10,5 +10,6 @@ hexSticker::sticker(
   url = "nathaneastwood/fpl",
   u_color = "#37003C",
   u_size = 2,
-  dpi = 1200
+  dpi = 1200,
+  asp = 0.9
 )
